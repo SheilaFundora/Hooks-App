35421c44474719e3c74dbacea8ee24c8c811b5d4
sheilaFundora <sheilafundora04@gamil.com>
2022-09-24 16:57:03 -0400

    initial master

---
+++ src/HookApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HookApp = () => {
+    return (
+        <div>
+          <h1 className="text-success mt-5 text-center">Hooks</h1>
+        </div>
+    );
+};
+
+export default HookApp;
